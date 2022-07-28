a20f38fede25563697f45fd9850259ca48dee0f6
Alejandro Zapata <alzapata317@gmail.com>
2022-07-27 21:29:25 -0400

    Added Inquirer prompts in order to view databases and add roles and employees to database

---
+++ server.js
@@ -0,0 +1,129 @@
+const inquirer = require('inquirer')
+
+function init() {
+    inquirer 
+        .prompt([
+            {
+                type: 'list',
+                name: 'Explore_Database',
+                choices: 
+                ['View all departments',
+                'View all roles',
+                'View all employees',
+                'Add a department',
+                'Add a role',
+                'add an employee',
+                'Update an employee role',
+                'Quit']
+            },
+        ])
+        .then((choice) => {
+            switch (choice.Explore_Database) {
+                case 'View all departments':
+                    viewDepartments()
+                    break;
+                case 'View all roles':
+                    viewRoles()
+                    break;
+                case 'View all employees':
+                    viewEmployees()
+                    break;
+                case 'Add a department':
+                    addDepartments()
+                    break;
+                case 'Add a role':
+                    addRole()
+                    break;
+                case 'add an employee':
+                    addEmployee()
+                    break;
+                case 'Update an employee role':
+                    updateEmployee()
+                    break;
+                case 'Quit':
+                    WantToExit
+                default: 'No value was Inputted'
+                    break;
+
+            }
+        })
+    function viewDepartments() {
+
+    }
+    function viewRoles() {
+
+    }
+    function viewEmployees() {
+
+    }
+    function addDepartments() {
+        inquirer
+            .prompt ([
+                {
+                    type: 'input',
+                    name: 'DepartmentName',
+                    message: 'What is the name of the department you would like to add?'
+                }
+                .then((answer) => {
+                })
+            ])
+    }
+    function addRole() {
+        inquirer
+            .prompt([
+                {
+                    type: 'input',
+                    name: 'RoleName',
+                    message: 'What it the name of the role?'
+                },
+                {
+                    type: 'input',
+                    name: 'RoleSalary',
+                    message: 'What is the salary for this role?'
+                },
+                {
+                    type: 'input',
+                    name: 'DepartmentName',
+                    message: 'What department does this role work in?'
+                }
+            ])
+            .then((answers) => {
+
+            })
+    }
+    function addEmployee() {
+        inquirer
+            .prompt([
+                {
+                    type: 'input',
+                    name: 'firstname',
+                    message: 'What is the first name of this employee?'
+                },
+                {
+                    type: 'input',
+                    name: 'lastname',
+                    message: 'What is the last name of this employee?'
+                },
+                {
+                    type: 'input',
+                    name: 'role',
+                    message: 'What is this employees role?'
+                },
+                {
+                    type: 'input',
+                    name: 'manager',
+                    message: 'Who is this employees manager?'
+                }
+            ])
+            .then((answers) => {
+
+            })
+    }
+    function updateEmployee() {
+
+    }
+    const WantToExit = () => {
+        return init();
+    }
+}
+init();
